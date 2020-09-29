5eb2810736ed20ae5777b1af2692860747362ec5
Vinícius Godoy de Mendonça <vinigodoy@gmail.com>
2020-09-29 13:29:14 -0300

    FPSCamera solution

---
+++ src/br/pucpr/mage/VectorUtil.java
@@ -0,0 +1,68 @@
+package br.pucpr.mage;
+
+import org.joml.Vector3f;
+import org.joml.Vector3fc;
+
+public class VectorUtil {
+    public static Vector3f add(Vector3fc a, Vector3f b) {
+        return new Vector3f(a).add(b);
+    }
+
+    public static Vector3f add(Vector3fc a, float x, float y, float z) {
+        return new Vector3f(a).add(x, y, z);
+    }
+
+    public static Vector3f sub(Vector3fc a, Vector3f b) {
+        return new Vector3f(a).sub(b);
+    }
+
+    public static Vector3f sub(Vector3fc a, float x, float y, float z) {
+        return new Vector3f(a).sub(x, y, z);
+    }
+
+    public static Vector3f mul(Vector3fc v, float s) {
+        return new Vector3f(s).mul(s);
+    }
+
+    public static Vector3f mul(Vector3fc a, Vector3fc b) {
+        return new Vector3f(a).mul(b);
+    }
+
+    public static Vector3f mul(Vector3fc a, float x, float y, float z) {
+        return new Vector3f(a).mul(x, y, z);
+    }
+
+    public static Vector3f cross(Vector3fc a, Vector3fc b) {
+        return new Vector3f(a).cross(b);
+    }
+
+    public static Vector3f cross(Vector3fc a, float x, float y, float z) {
+        return new Vector3f(a).cross(x, y, z);
+    }
+
+    public static Vector3f normalize(Vector3fc v) {
+        return new Vector3f(v).normalize();
+    }
+
+    public static float dot(Vector3fc a, Vector3fc b) {
+        return a.dot(b);
+    }
+
+    public static float dot(Vector3fc a, float x, float y, float z) {
+        return a.dot(x, y, z);
+    }
+
+    public static Vector3f negate(Vector3fc vec) {
+        return new Vector3f(vec);
+    }
+
+    public static Vector3f lerp(Vector3fc a, Vector3fc b, float t) {
+        return a.lerp(b, t, new Vector3f());
+    }
+
+    public static Vector3f reflect(Vector3fc ray, Vector3fc normal) {
+        return ray.reflect(normal, new Vector3f());
+    }
+
+
+}
---
+++ src/br/pucpr/mage/camera/FPSCamera.java
@@ -0,0 +1,47 @@
+package br.pucpr.mage.camera;
+
+import org.joml.Vector3f;
+
+import static br.pucpr.mage.VectorUtil.*;
+
+public class FPSCamera extends Camera {
+    private float angleY = 0;
+
+    public Vector3f getDirection() {
+        return new Vector3f(0, 0, 1).rotateY(angleY);
+    }
+
+    @Override
+    public Vector3f getTarget() {
+        return add(getPosition(), getDirection());
+    }
+
+    public FPSCamera setAngleY(float angle) {
+        this.angleY = angle;
+        return this;
+    }
+
+    public FPSCamera turn(float angle, float speed) {
+        this.angleY += angle * speed;
+        return this;
+    }
+
+    public FPSCamera move(float speed, float time, boolean strafe) {
+        return strafe ? strafe(speed, time) : move(speed, time);
+    }
+
+    public FPSCamera move(float speed, float time) {
+        var displacement = mul(getDirection(), speed * time);
+        getPosition().add(displacement);
+        return this;
+    }
+
+    public FPSCamera strafe(float speed, float time) {
+        var strafe =
+                cross(getDirection(), getUp())
+                .mul(speed * time);
+        getPosition().add(strafe);
+        return this;
+
+    }
+}
